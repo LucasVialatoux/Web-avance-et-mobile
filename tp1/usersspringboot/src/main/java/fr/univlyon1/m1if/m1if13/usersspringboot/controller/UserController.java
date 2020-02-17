7d0382e24a239b890e300d38862646ea1eca12fc
Vialatoux Lucas <lucas.vialatoux@etu.univ-lyon1.fr>
2020-02-17 19:18:14 +0100

    Début ThymeLeaf

--- tp1/usersspringboot/src/main/java/fr/univlyon1/m1if/m1if13/usersspringboot/controller/UserController.java
+++ tp1/usersspringboot/src/main/java/fr/univlyon1/m1if/m1if13/usersspringboot/controller/UserController.java
@@ -3,12 +3,15 @@ package fr.univlyon1.m1if.m1if13.usersspringboot.controller;
 import fr.univlyon1.m1if.m1if13.usersspringboot.model.User;
 import fr.univlyon1.m1if.m1if13.usersspringboot.DAO.UserDao;
 import java.util.Optional;
+import org.springframework.http.MediaType;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
 
 /**
  *
@@ -19,14 +22,38 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class UserController {
     private UserDao u = UserDao.createInstance();
+    
     /**
     * GET request
      * @param id
      * @return 
     */
-    @GetMapping("/users")
+    @GetMapping(value="/users")
     public Optional<User> get(@RequestParam(value = "id") String id){
         return u.get(id);
+    }
+    
+    /**
+    * GET request
+     * @param id
+     * @return 
+    */
+    @GetMapping(value="/users",produces=MediaType.APPLICATION_XML_VALUE)
+    public Optional<User> getXML(@RequestParam(value = "id") String id){
+        return u.get(id);
+    }
+
+    /**
+     *
+     * @param id
+     * @return
+     */
+    @GetMapping(value="/users",produces=MediaType.TEXT_HTML_VALUE)
+    public void getHTML (@RequestParam(value = "id") String id, Model model) {
+        Optional<User> user = u.get(id);
+        User us = user.get();
+        model.addAttribute("login", us.getLogin());
+        model.addAttribute("connected", us.isConnected());
     }
     
     /**
