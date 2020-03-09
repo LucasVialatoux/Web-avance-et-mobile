3575bab189f44f3f5c07fa7b04f4f2aea047666d
Paul Horcholle <paul.horcholle@etu.univ-lyon1.fr>
2020-03-09 22:09:43 +0100

    Ajout du client et des requetes

--- tp1/usersspringboot/src/main/java/fr/univlyon1/m1if/m1if13/usersspringboot/UsersspringbootApplication.java
+++ tp1/usersspringboot/src/main/java/fr/univlyon1/m1if/m1if13/usersspringboot/UsersspringbootApplication.java
@@ -2,6 +2,9 @@ package fr.univlyon1.m1if.m1if13.usersspringboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class UsersspringbootApplication {
@@ -10,4 +13,14 @@ public class UsersspringbootApplication {
 		SpringApplication.run(UsersspringbootApplication.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+				.allowedOrigins("http://localhost", "http://192.168.75.38");
+			}
+		};
+	}
 }
