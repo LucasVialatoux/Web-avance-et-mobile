7aecd5bccf2b8b88857d2185093b2ce571514980
Paul Horcholle <paul.horcholle@etu.univ-lyon1.fr>
2020-03-17 17:38:16 +0100

    Ajout d'axios et requetage vers le serveur spring fonctionnel

--- app/server/routes/admin.js
+++ app/server/routes/admin.js
@@ -1,9 +1,12 @@
 var express = require('express')
 var router = express.Router()
+const axios = require('axios')
 
 // Route page d'administration
-router.get('/', function (req, res) {
-  res.render('admin')
+router.get('/', async (req, res) => {
+  let response = await axios.get('http://localhost:8080/users')
+
+  res.render('admin', {users: response.data})
 })
 
 module.exports = router
