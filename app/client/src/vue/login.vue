084db72ea616dbefb6225902fce3c78d89afdd76
Horcholle Paul <phorcholle@geomod.fr>
2020-05-28 06:49:09 +0200

    Connexion au serveur user

--- app/client/src/vue/login.vue
+++ app/client/src/vue/login.vue
@@ -1,23 +1,34 @@
 <template>
 <div>
-    <form>
-        <div>
-            <label for="login">Login</label>
-            <input type="text" id="login" name="login" v-model="login"/>
-        </div>
-        <div>
-            <label for="mdp">Mdp</label>
-            <input type="password" id="mdp" name="mdp" v-model="mdp"/>
-        </div>
-        <button>Login</button>
-    </form>
+    <div>
+        <label for="login">Login</label>
+        <input type="text" id="login" name="login" v-model="login"/>
+    </div>
+    <div>
+        <label for="mdp">Mdp</label>
+        <input type="password" id="mdp" name="mdp" v-model="mdp"/>
+    </div>
+    <button @click="loginPost()">Login</button>
 </div>
 </template>
 
 <script>
+const axios = require('axios')
+
 export default {
-    name: "login",
+    name: 'login',
     props: ['login', 'mdp'],
+    methods: {
+        loginPost() {
+            axios.post(`${window.origin}/auth/login?login=${this.login}&password=${this.mdp}`)
+            .then(response => {
+                console.log(response)
+            })
+            .catch(error => {
+                console.log(error)
+            })
+        }
+    }
 }
 </script>
 
